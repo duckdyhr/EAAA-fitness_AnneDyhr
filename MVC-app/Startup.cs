2330492859140631d85835b736be92e4f927617d
duckdyhr <duckdyhr@hotmail.com>
2016-05-23 12:48:16 +0200

    Begynder dag 02

---
+++ MVC-app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_app.Startup))]
+namespace MVC_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
